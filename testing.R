5d6c9d2cdf2e5e3b5acd6a1ec4b03ef0585051e1
Sukerfish <viper31jas@aim.com>
2022-12-12 17:34:55 -0500

    yo counter work

--- testing.R
+++ testing.R
@@ -11,10 +11,35 @@ library(gsw)
 
 #library(Cairo)   # For nicer ggplot2 output when deployed on Linux
 
+#library(lubridate)
+
+# missionNumber <- "output"
+# 
+# head <- read.csv(paste0(missionNumber,".ssv"),
+#                  sep="", #whitespace as delimiter
+#                  nrows=1,
+#                  skip=500)
+# 
+# raw <- read.csv(paste0(missionNumber,".ssv"),
+#                 sep="", #whitespace as delimiter
+#                 skip=503,
+#                 header = FALSE)
+# 
+# colnames(raw) <- colnames(head)
+# 
+# raw <- raw %>%
+#   mutate(m_present_time = as_datetime(m_present_time)) #convert to POSIXct
+# 
+# raw$m_present_time <- as_datetime(floor(seconds(raw$m_present_time)))
+
 # fileList <- list.files(path = ".",
 #            pattern = "*.RData")
 
-load("M120.RData")
+glider <- read_rds("M118.rds")
+# glider$m_present_time <- as_datetime(floor(seconds(glider$m_present_time)))
+# 
+# glider <- glider %>%
+#   left_join(raw)
 
 #https://rdrr.io/github/AustralianAntarcticDivision/ZooScatR/src/R/soundvelocity.R
 c_Coppens1981 <- function(D,S,T){
@@ -40,15 +65,15 @@ input <- glider %>%
   colnames()
 
 chunk <- glider %>%
-  filter(m_present_time >= "2022-10-15" & m_present_time < "2022-11-03") %>%
+  filter(m_present_time >= "2022-08-15" & m_present_time < "2022-08-19") %>%
   mutate(status = if_else(m_avg_depth_rate > 0, "dive", "climb")) %>%
   fill(status) %>%
   #filter(status == "dive") %>%
   mutate(soundvel1 = c_Coppens1981(sci_rbrctd_depth_00,
                                    sci_rbrctd_salinity_00,
-                                   sci_rbrctd_temperature_00)) %>%
-  mutate(conTemp = gsw_CT_from_t(sci_rbrctd_salinity_00, sci_rbrctd_temperature_00, sci_rbrctd_pressure_00)) %>%
-  mutate(soundvel2 = gsw_sound_speed(sci_rbrctd_salinity_00, conTemp, sci_rbrctd_pressure_00))
+                                   sci_rbrctd_temperature_00))
+  #mutate(conTemp = gsw_CT_from_t(sci_rbrctd_salinity_00, sci_rbrctd_temperature_00, sci_rbrctd_pressure_00)) %>%
+  #mutate(soundvel2 = gsw_sound_speed(sci_rbrctd_salinity_00, conTemp, sci_rbrctd_pressure_00))
   
 # pings <- chunk %>%
 #   filter(!is.na(m_water_depth)) %>%
@@ -60,8 +85,32 @@ chunkSummary <- chunk %>%
             
 zunk <- chunk %>%
   select(all_of(input)) %>%
-  mutate(across(m_roll, zscore = ((avg - LTmean)/stdev))))
+  mutate(across(m_roll, zscore = ((avg - LTmean)/stdev)))
   
+#glider$m_present_time <- as_datetime(floor(seconds(glider$m_present_time)))
+yunk <- glider %>%
+  #filter(m_present_time >= "2022-08-15" & m_present_time < "2022-08-19") %>%
+  select(m_present_time, sci_water_pressure, m_depth, c_ballast_pumped) %>%
+  filter(m_depth > 1) %>%
+  filter(!is.nan(sci_water_pressure)) %>%
+  filter(sci_water_pressure > 0) %>%
+  mutate(check = sci_water_pressure - lead(sci_water_pressure)) %>%
+  mutate(status2 = if_else(check < 0, 1, 2)) %>%
+  mutate(check2 = abs(status2 - lead(status2))) 
+  mutate()
+  select(m_present_time, status2)
+
+#ballast pump delta method + max pitch ... at surface only
+qunk <- glider %>%
+  select(m_present_time, sci_water_pressure, m_depth, c_ballast_pumped, c_battpos) %>%
+  filter(!is.nan(c_ballast_pumped)) %>%
+  filter(!is.nan(c_battpos)) %>%
+  #mutate(check == 0) %>%
+  mutate(check2 = ifelse(c_battpos == max(c_battpos, na.rm = TRUE), 1, 0)) %>%
+  #filter(surface == 1)
+
+  mutate(check = c_ballast_pumped - lead(c_ballast_pumped)) %>%
+  filter(abs(check) >= 100)
 
 # ts_plot(chunk %>%
 #           filter(!is.na(sci_water_temp)),
