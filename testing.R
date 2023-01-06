aa2d9a12e20ac05b77f79cadaf08bd46d369ce06
Sukerfish <viper31jas@aim.com>
2023-01-06 14:03:31 -0500

    Sal and sv calculation tests

--- testing.R
+++ testing.R
@@ -50,6 +50,37 @@ c_Coppens1981 <- function(D,S,T){
   return(c)
 }
 
+ec2pss <-
+  function (ec, t, p = 0) {
+    # Define conductivity ratio
+    R <- ec/42.914
+    
+    # Estimate temperature correction (valid for -2 < t < 35)
+    c <- c(0.6766097, 0.0200564, 0.0001104259, -6.9698e-07, 1.0031e-09)
+    rt <- c[1] + c[2] * t + c[3] * t^2 + c[4] * t^3 + c[5] * t^4
+    
+    # Estimate pressure correction (validity range varies with t and S)
+    d <- c(0.03426, 0.0004464, 0.4215, -0.003107)
+    e <- c(2.07e-05, -6.37e-10, 3.989e-15)
+    Rp <- 1 + p * (e[1] + e[2] * p + e[3] * p^2)/(1 + d[1] * t + 
+                                                    d[2] * t^2 + (d[3] + d[4] * t) * R)
+    
+    # Estimate salinity (valid for 2 < S < 42 and -2 < t < 35).       
+    Rt <- R/(Rp * rt)
+    a <- c(0.008, -0.1692, 25.3851, 14.0941, -7.0261, 2.7081)
+    b <- c(5e-04, -0.0056, -0.0066, -0.0375, 0.0636, -0.0144)
+    ft <- (t - 15)/(1 + 0.0162 * (t - 15))
+    S <- a[1] + a[2] * Rt^0.5 + a[3] * Rt + a[4] * Rt^1.5 + a[5] * 
+      Rt^2 + a[6] * Rt^2.5 + ft * (b[1] + b[2] * Rt^0.5 + b[3] * 
+                                     Rt + b[4] * Rt^1.5 + b[5] * Rt^2 + b[6] * Rt^2.5)
+    
+    # Estimate salinity correction for S < 2
+    x <- 400 * Rt
+    y <- 100 * Rt
+    ifelse(S >= 2, S, S - a[1]/(1 + 1.5 * x + x^2) - b[1] * ft/(1 + 
+                                                                  y^0.5 + y + y^1.5))
+  }
+
 #pull out science variables
 scivars <- glider %>%
   select(starts_with("sci")) %>%
@@ -65,13 +96,14 @@ input <- glider %>%
   colnames()
 
 chunk <- glider %>%
-  filter(m_present_time >= "2022-08-15" & m_present_time < "2022-08-19") %>%
+  filter(m_present_time >= "2022-8-15" & m_present_time < "2022-8-23") %>%
   mutate(status = if_else(m_avg_depth_rate > 0, "dive", "climb")) %>%
   fill(status) %>%
   #filter(status == "dive") %>%
-  mutate(soundvel1 = c_Coppens1981(sci_rbrctd_depth_00,
-                                   sci_rbrctd_salinity_00,
-                                   sci_rbrctd_temperature_00))
+  mutate(osg_salinity = ec2pss(sci_water_cond*10, sci_water_temp, sci_water_pressure*10)) %>%
+  mutate(soundvel1 = c_Coppens1981(m_depth,
+                                   osg_salinity,
+                                   sci_water_temp))
   #mutate(conTemp = gsw_CT_from_t(sci_rbrctd_salinity_00, sci_rbrctd_temperature_00, sci_rbrctd_pressure_00)) %>%
   #mutate(soundvel2 = gsw_sound_speed(sci_rbrctd_salinity_00, conTemp, sci_rbrctd_pressure_00))
   
