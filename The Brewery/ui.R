5e48aa90a60aa619e8d607ed226033cdb4605ef0
Sukerfish <47607889+Sukerfish@users.noreply.github.com>
2023-02-07 22:52:29 -0500

    UI rearrangement and code cleanup

--- The Brewery/ui.R
+++ The Brewery/ui.R
@@ -1,155 +1,160 @@
 # Define UI for application that draws a histogram
-fluidPage(
+navbarPage(
+  title = "The Brewery",
   #force notifications to center of page
-  tags$head(
-    tags$style(
-      HTML(".shiny-notification {
+  tags$head(tags$style(
+    HTML(
+      ".shiny-notification {
              position:fixed;
              top: calc(50%);
              left: calc(50%);
              }
              "
-      )
     )
-  ),
-  titlePanel("The Brewery"),
-  wellPanel(
-    #start button
-    actionButton("load", "Load Mission Data", icon("arrows-rotate"), 
-                 style="color: #fff; background-color: #963ab7; border-color: #2e6da4"),
-    selectInput(
-      "mission",
-      "Which mission data to display",
-      choices = c(missionList),
-      selected =  NULL
-    ),
-    #parameter input row
-    fluidRow(
-      column(
-        4,
-        dateInput(
-          "date1",
-          "Start Date:",
-          value = NULL,
-          min = NULL,
-          max = NULL,
-          format = "mm/dd/yy"
-        ),
-        dateInput(
-          "date2",
-          "End Date:",
-          value = NULL,
-          min = NULL,
-          max = NULL,
-          format = "mm/dd/yy"
-        )
-      ),
-      column(
-        4,
-        numericInput("min_depth", "Depth Minimum", 3, min = 0, max = 1000),
-        numericInput(
-          "max_depth",
-          "Depth Maximum",
-          150,
-          min = 0,
-          max = 1000
-        )
-      ),
-      column(
-        4,
-        checkboxGroupInput(
-          "status",
-          "Dive only?",
-          choices = c("dive" = "dive",
-                      "climb" = "climb"),
-          selected = c("dive")
-        )
-      )
-    )),
-  actionButton("initialize", "Visualize", icon("plane"), 
-               style="color: #fff; background-color: #337ab7; border-color: #2e6da4"),
-  #science variable settings
-  tabsetPanel(
-    tabPanel("Map",
-             fluidRow(
-               leafletOutput("missionmap")
-             ), ),
-    tabPanel("Science Data",
-             fluidRow(
-               column(
-                 3,
-                 wellPanel(
-                   selectInput(
-                     "display_var",
-                     "Which science variable to display",
-                     choices = NULL
-                   ),
-                   numericInput("min", "Sci Axis Minimum", NULL, min = 1, max = 100),
-                   numericInput("max", "Sci Axis Maximum", NULL, min = 1, max = 100)
-                 )
-               ),
-               column(
-                 9,
-                 h4("Brush and double-click to zoom (double-click again to reset)"),
-                 plotOutput(
-                   "sciplot",
-                   dblclick = "sciplot_dblclick",
-                   brush = brushOpts(id = "sciplot_brush",
-                                     resetOnNew = TRUE)
-                 )
-               ))
-    ),
-    #flight variable settings
-    tabPanel("Flight Data",
-             fluidRow(
-               column(3,
-                      wellPanel(
-                        #   selectInput(
-                        #     "flight_var",
-                        #     "Which flight variable(s) to display",
-                        #     choices = c(flightvars),
-                        #     selected = c("m_roll")
-                        #   )
-                        # )),
-                        selectizeInput("flight_var",
-                                       "Which flight variable(s) to display",
-                                       choices = NULL,
-                                       multiple = TRUE,
-                                       options = list(plugins= list('remove_button'))))),
-               column(
-                 9,
-                 h4("Brush and double-click to zoom (double-click again to reset)"),
-                 plotOutput(
-                   "flightplot",
-                   dblclick = "flightplot_dblclick",
-                   brush = brushOpts(id = "flightplot_brush",
-                                     resetOnNew = TRUE)
-                 )
-               )
-             )),
-    #sound velocity tab
-    tabPanel("Sound Velocity",
-             fluidRow(
-               column(3,
-                      wellPanel(
-                        numericInput(
-                          "soundmin",
-                          "Sound Axis Minimum",
-                          NULL,
-                          min = 1,
-                          max = 100
-                        ),
-                        numericInput(
-                          "soundmax",
-                          "Sound Axis Maximum",
-                          NULL,
-                          min = 1,
-                          max = 100
-                        )
-                      )),
-               column(9,
-                      plotOutput("soundplot"))
-             ), ),
-    #sound velocity tab
-  )
+  )),
+  tabPanel(title = "Mission Plotting",
+           fluidPage(
+             #parameter input row
+             sidebarLayout(
+               sidebarPanel(
+                 actionButton(
+                   inputId = "load",
+                   label = "Load Mission Data",
+                   icon("arrows-rotate"),
+                   style =
+                     "color: #fff; background-color: #963ab7; border-color: #2e6da4"
+                 ),
+                 selectInput(
+                   inputId = "mission",
+                   label = "Which mission data to display",
+                   choices = c(missionList),
+                   selected =  NULL
+                 ),
+                 actionButton(
+                   inputId = "initialize",
+                   label = "Visualize",
+                   icon("plane"),
+                   style = "color: #fff; background-color: #337ab7; border-color: #2e6da4"
+                 ),
+                 dateInput(
+                   inputId = "date1",
+                   label = "Start Date:",
+                   value = NULL,
+                   min = NULL,
+                   max = NULL,
+                   format = "mm/dd/yy"
+                 ),
+                 dateInput(
+                   inputId = "date2",
+                   label = "End Date:",
+                   value = NULL,
+                   min = NULL,
+                   max = NULL,
+                   format = "mm/dd/yy"
+                 ),
+                 numericInput(
+                   inputId = "min_depth",
+                   label = "Depth Minimum",
+                   value = 3,
+                   min = 0,
+                   max = 1000
+                 ),
+                 numericInput(
+                   inputId = "max_depth",
+                   label = "Depth Maximum",
+                   value = 150,
+                   min = 0,
+                   max = 1000
+                 ),
+                 checkboxGroupInput(
+                   inputId = "status",
+                   label = "Dive only?",
+                   choices = c("dive" = "dive",
+                               "climb" = "climb"),
+                   selected = c("dive")
+                 ),
+               width = 2),
+               mainPanel(#science variable settings
+                 tabsetPanel(
+                   tabPanel(title = "Map",
+                            fluidRow(leafletOutput(outputId = "missionmap")),),
+                   tabPanel(title = "Science Data",
+                            fluidRow(
+                              column(3,
+                                     wellPanel(
+                                       selectInput(
+                                         inputId = "display_var",
+                                         label = "Which science variable to display",
+                                         choices = NULL
+                                       ),
+                                       numericInput(inputId = "min",
+                                                    label = "Sci Axis Minimum",
+                                                    NULL),
+                                       numericInput(inputId = "max",
+                                                    label = "Sci Axis Maximum",
+                                                    NULL)
+                                     )),
+                              column(
+                                9,
+                                h4("Brush and double-click to zoom (double-click again to reset)"),
+                                plotOutput(
+                                  outputId = "sciplot",
+                                  dblclick = "sciplot_dblclick",
+                                  brush = brushOpts(id = "sciplot_brush",
+                                                    resetOnNew = TRUE)
+                                )
+                              )
+                            )),
+                   #flight variable settings
+                   tabPanel(title = "Flight Data",
+                            fluidRow(
+                              column(3,
+                                     wellPanel(
+                                       #   selectInput(
+                                       #     "flight_var",
+                                       #     "Which flight variable(s) to display",
+                                       #     choices = c(flightvars),
+                                       #     selected = c("m_roll")
+                                       #   )
+                                       # )),
+                                       selectizeInput(
+                                         inputId = "flight_var",
+                                         label = "Which flight variable(s) to display",
+                                         choices = NULL,
+                                         multiple = TRUE,
+                                         options = list(plugins = list('remove_button'))
+                                       )
+                                     )),
+                              column(
+                                9,
+                                h4("Brush and double-click to zoom (double-click again to reset)"),
+                                plotOutput(
+                                  outputId = "flightplot",
+                                  dblclick = "flightplot_dblclick",
+                                  brush = brushOpts(id = "flightplot_brush",
+                                                    resetOnNew = TRUE)
+                                )
+                              )
+                            )),
+                   #sound velocity tab
+                   tabPanel(title = "Sound Velocity",
+                            fluidRow(
+                              column(3,
+                                     wellPanel(
+                                       numericInput(inputId = "soundmin",
+                                                    label = "Sound Axis Minimum",
+                                                    NULL),
+                                       numericInput(inputId = "soundmax",
+                                                    label = "Sound Axis Maximum",
+                                                    NULL)
+                                     )),
+                              column(9,
+                                     plotOutput(outputId = "soundplot"))
+                            ),),
+                   selected = "Map"
+                 ),width = 10)
+             )
+           )),
+  tabPanel(title = "Data Import", ),
+  tabPanel(title = "Development"),
 )
