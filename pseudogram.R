94f0ce9ebdcffaaa3ba4e909527c13726ff23a68
Sukerfish <viper31jas@aim.com>
2023-03-20 17:01:20 -0400

    first functional plot

--- pseudogram.R
+++ pseudogram.R
@@ -1,6 +1,10 @@
 library(tidyverse)
 library(ggplot2)
 library(lubridate)
+library(zoo)
+#library(echogram)
+
+#test <- palette.echogram(Svthr = -75, Svmax = -35, col.sep = 1, scheme = "echov", visu = FALSE)
 
 raw <- read.csv("usf-stella-2023-059-1-194.ssv",
                 sep="", #whitespace as delimiter
@@ -12,41 +16,87 @@ rawDepth <- read.csv("./depths/usf-stella-2023-059-1-194.ssv",
                 skip=17,
                 header = FALSE)
 
+#force as dataframe and rename
 df <- as.data.frame(raw) %>%
   rename(m_present_time = V1)
-df$m_present_time <- as_datetime(df$m_present_time)
+#cutoff at seconds for clean merge
+df$m_present_time <- as_datetime(floor(seconds(df$m_present_time)))
 
+#tbd time correction
+df <- df %>%
+  mutate(m_present_time = m_present_time - 20)
+
+#force as dataframe and rename
 ef<- as.data.frame(rawDepth) %>%
   rename(m_present_time = V2) %>%
   rename(m_depth = V1)
-ef$m_present_time <- as_datetime(ef$m_present_time)
+#cutoff at seconds for clean merge
+ef$m_present_time <- as_datetime(floor(seconds(ef$m_present_time)))
 
-bigLong <- pivot_longer(df, !c(m_present_time))
+#m_depth interpolation (sbd sourced)
+full.time <- with(ef,seq(m_present_time[1],tail(m_present_time,1),by=1)) #grab full list of timestamps
+depth.zoo <- zoo(ef[1], ef$m_present_time) #convert to zoo
+result <- na.approx(depth.zoo, xout = full.time) #interpolate
 
+idepth <- fortify.zoo(result) %>% #extract out as DF
+  rename(i_depth = m_depth) %>%
+  rename(m_present_time = Index) %>%
+  mutate(m_present_time = as_datetime(m_present_time))
 
+#force both time sets to match (i.e., round to 1sec)
+idepth$m_present_time <- as_datetime(floor(seconds(idepth$m_present_time)))
+
+# SSV output for testing
+# output <- idepth
+# output$m_present_time <- as.numeric(output$m_present_time)
+# write.table(output,
+#             file =  "test.ssv",
+#             quote = FALSE,
+#             row.names = FALSE,
+#             sep = " ")
+
+#merge interpolated depth
+bigLong <- pivot_longer(df, !c(m_present_time)) %>%
+  left_join(idepth)
+
+#build ping depth matrix
+binList <- unique(bigLong$name)
+binOffset <- data.frame(name = c(binList),
+                   offset = c(seq(0, (length(binList)-1)*2.5, 2.5)))
+
+#merge in ping depths and compute
+bigLong <- bigLong %>%
+  left_join(binOffset) %>%
+  mutate(p_depth = i_depth + offset)
+
+#plot
 ggplot(data = 
          bigLong,
        aes(x=m_present_time,
-           y=m_depth,
-           z=decibels)) +
+           y=p_depth,
+           z=value)) +
   geom_point(
-    aes(color = decibels),
+    aes(color = value),
+    size = 4,
+    pch = 15,
     na.rm = TRUE
   ) +
   #coord_cartesian(xlim = rangesci$x, ylim = rangesci$y, expand = FALSE) +
-  #geom_hline(yintercept = 0) +
   scale_y_reverse() +
-  scale_colour_viridis_c(limits = c(input$min, input$max)) +
+  # scale_colour_manual(values = c(test$palette),
+  #                     breaks = c(test$breaks))
+  scale_colour_viridis_c(limits = c(min(bigLong$value), max(bigLong$value)),
+                         #option = "C"
+                         ) +
   # geom_point(data = filter(chunk(), m_water_depth > 0),
   #            aes(y = m_water_depth),
   #            size = 0.1,
   #            na.rm = TRUE
   # ) +
   theme_bw() +
-  # labs(title = paste0(missionNum, " Science Data"),
-  #      y = "Depth (m)",
-  #      x = "Date") +
+  labs(title = "EK",
+       y = "Depth (m)",
+       x = "Date") +
   theme(plot.title = element_text(size = 32)) +
   theme(axis.title = element_text(size = 16)) +
   theme(axis.text = element_text(size = 12))
-})
