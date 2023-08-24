54d73beeb66451ae2f99bc1102bb78effe65e66e
Sukerfish <viper31jas@aim.com>
2023-08-24 16:34:42 -0400

    finish header formatting, seems to work

--- gliderSVP.R
+++ gliderSVP.R
@@ -1,6 +1,8 @@
 ### sound velocity publishing ###
 
 library(tidyverse)
+library(lubridate)
+library(stringr)
 
 my.write <- function(x, file, header, f = write.csv, ...){
   # create and open the file connection
@@ -35,26 +37,31 @@ for (i in deployedGliders$Name){
   svpdf <- gliderdf %>%
     select(c(m_present_time, segment, i_lat, i_lon, 
              osg_i_depth, osg_soundvel1,
-             cast, yo_id)) #select only vars of interest
+             cast, yo_id, m_water_depth)) #select only vars of interest
   
   out <- list() #initialize list for segment by segment calcs
   for (j in unique(svpdf$segment)) {
     segment <- j %>%
-      str_remove(pattern = ".ssv")
+      str_remove(pattern = ".ssv") #get segment identifier
     
     out[[segment]] <- svpdf %>%
       filter(segment == j) %>%
-      filter(yo_id > 0) %>%
-      filter(cast == "Downcast" | cast == "Upcast") %>%
-      group_by(yo_id, cast) %>%
+      filter(yo_id > 0) %>% #check if yo was ID'd
+      filter(cast == "Downcast" | cast == "Upcast") %>% #ensure no surface/unknown intervals
+      group_by(yo_id, cast) %>% #calculate averages per profile
       mutate(p_lat = mean(i_lat, na.rm = TRUE),
              p_lon = mean(i_lon, na.rm = TRUE),
-             p_time = mean(m_present_time, na.rm = TRUE)) %>%
+             p_time = mean(m_present_time, na.rm = TRUE),
+             p_min_depth = min(osg_i_depth, na.rm = TRUE),
+             p_max_depth = max(osg_i_depth, na.rm = TRUE)) %>%
       ungroup() %>%
-      group_by(yo_id) %>%
+      group_by(yo_id) %>% #calculate averages per yo
       mutate(y_lat = mean(i_lat, na.rm = TRUE),
              y_lon = mean(i_lon, na.rm = TRUE),
-             y_time = mean(m_present_time, na.rm = TRUE)) %>%
+             y_time = mean(m_present_time, na.rm = TRUE),
+             #y_water_depth = mean(filter(., m_water_depth > 0), na.rm = TRUE),
+             y_min_depth = min(osg_i_depth, na.rm = TRUE),
+             y_max_depth = max(osg_i_depth, na.rm = TRUE)) %>%
       ungroup()
     
   }
@@ -74,16 +81,25 @@ for (i in deployedGliders$Name){
         yop <- data.frame()
         yop <- castp %>%
           filter(yo_id == y) %>%
-          select(c(osg_i_depth, osg_soundvel1)) %>%
-          filter(!is.na(osg_soundvel1)) #select vars of interest and remove NAs
+          filter(!is.na(osg_soundvel1))  #remove NAs
+          #arrange(osg_i_depth) #save as top to bottom?
+        
+        yodat <- head(yop, 1)
+        
+        #format metadata for header in .asvp format
+        metadata <- paste0("( SoundVelocity 1.00 ", y, " ", strftime(Sys.time(), format = "%Y%m%d%H%M"), " ", #file ID/creation time
+                           round(yodat$p_lat, 7), " ", round(yodat$p_lon, 7), " ", #profile lat/lon
+                           "4500", " ", #radius of validity
+                           strftime(min(yop$m_present_time), format = "%Y%m%d%H%M", tz = "UTC"), #valid start time, force m_present_time as UTC
+                           " ", strftime(max(yop$m_present_time), format = "%Y%m%d%H%M", tz = "UTC"), " ", #valid end time
+                           i, " PE ", nrow(yop), " )") #platform name, history of modifications, number of samples
         
         #write csv for each yo within cast within segment
-        my.write(yop, paste0("/echos/gvp/", i, "/", l, "/", k, "_yo_", y, ".csv"), "test header", f = write.csv, row.names = FALSE)
-        # datafile <- file(paste0("/echos/gvp/", i, "/", l, "/", k, "_yo_", y, ".csv"), open = "wt")
-        # writeLines("testing", con=datafile)
-        # write.csv(yop, datafile)
-        # 
-        # close(datafile)
+        my.write(yop %>%
+                   select(c(osg_i_depth, osg_soundvel1)) %>%
+                   round(., 4), 
+                 paste0("/echos/gvp/", i, "/", l, "/", k, "_yo_", y, ".asvp"), header = metadata, 
+                 f = write.table, row.names = FALSE, col.names = FALSE)
       }
         
     }
@@ -93,33 +109,33 @@ for (i in deployedGliders$Name){
 }
   
 
-  plot <- 
-    ggplot(
-      data = out[[segment]],
-      aes(x = osg_soundvel1,
-          y = osg_i_depth,
-          color = cast,
-          #shape = variable
-      )) +
-    geom_point_interactive(size = 3,
-                           pch = 1) +
-    # scale_color_gradient(
-    #   low = "red",
-    #   high = "blue",
-    # ) +
-    #coord_cartesian(xlim = rangefli$x, ylim = rangefli$y, expand = FALSE) +
-    scale_y_reverse() +
-    theme_bw() +
-    labs(title = paste0(segment, " SVP"),
-         x = "Sound Velocity (m/s)",
-         y = "Depth (m)",
-         #color = "Time",
-         #caption = "Red = older ... Blue = more recent",
-         #caption = "<img src='./www/cms_horiz.png' width='200'/>"
-    ) +
-    theme(plot.title = element_text(size = 32),
-          axis.title = element_text(size = 16),
-          axis.text = element_text(size = 12),
-          plot.caption = element_markdown(),
-          #legend.position ="none",
-    ) 
+  # plot <- 
+  #   ggplot(
+  #     data = out[[segment]],
+  #     aes(x = osg_soundvel1,
+  #         y = osg_i_depth,
+  #         color = cast,
+  #         #shape = variable
+  #     )) +
+  #   geom_point_interactive(size = 3,
+  #                          pch = 1) +
+  #   # scale_color_gradient(
+  #   #   low = "red",
+  #   #   high = "blue",
+  #   # ) +
+  #   #coord_cartesian(xlim = rangefli$x, ylim = rangefli$y, expand = FALSE) +
+  #   scale_y_reverse() +
+  #   theme_bw() +
+  #   labs(title = paste0(segment, " SVP"),
+  #        x = "Sound Velocity (m/s)",
+  #        y = "Depth (m)",
+  #        #color = "Time",
+  #        #caption = "Red = older ... Blue = more recent",
+  #        #caption = "<img src='./www/cms_horiz.png' width='200'/>"
+  #   ) +
+  #   theme(plot.title = element_text(size = 32),
+  #         axis.title = element_text(size = 16),
+  #         axis.text = element_text(size = 12),
+  #         plot.caption = element_markdown(),
+  #         #legend.position ="none",
+  #   ) 
