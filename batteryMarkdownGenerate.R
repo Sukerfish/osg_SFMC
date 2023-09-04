a7a7d0a0d270851f79037c9896fe4088d8aad549
Sukerfish <47607889+Sukerfish@users.noreply.github.com>
2023-09-03 20:25:10 -0400

    add option for alternate format for no amphr

--- batteryMarkdownGenerate.R
+++ batteryMarkdownGenerate.R
@@ -21,12 +21,18 @@ for (i in deployedGliders$Name){
   #load latest live data file
   load(paste0("/echos/", i, "/glider_live.RData"))
   
-  
+  if (ahrCap$ahrCap > 0){
     msg <- envelope() %>%
       emayili::render("/echos/batteryMarkdown.Rmd") %>%
       subject(paste0("Daily summary for ", as.character(i)))
     
     capture.output(print(msg, details = TRUE), file = paste0("/echos/", i, "/summary.html"))
-  
+  } else {
+    msg <- envelope() %>%
+      emayili::render("/echos/batteryMarkdownNoAmp.Rmd") %>%
+      subject(paste0("Daily summary for ", as.character(i)))
+    
+    capture.output(print(msg, details = TRUE), file = paste0("/echos/", i, "/summary.html"))
+  }
   
 }
