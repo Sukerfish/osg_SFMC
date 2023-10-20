931b0b53b2340b2c00143901c21f199e1c2bb49a
Sukerfish <viper31jas@aim.com>
2023-10-20 13:31:41 -0400

    load glider_live.RData into new environment each time

--- batteryMarkdownGenerate.R
+++ batteryMarkdownGenerate.R
@@ -12,24 +12,19 @@ colnames(deployedGliders)[2] = "ahrCap"
 deployedGliders <- deployedGliders %>%
   filter(!str_starts(Name,"#")) #remove any commented lines
 
-#initialize list
-#gliders_live <- list()
 for (i in deployedGliders$Name){
-  
-  rm(list = setdiff(ls(), c("i", "deployedGliders")))
-  
-  #load latest live data file
-  load(paste0("/echos/", i, "/glider_live.RData"))
+  #load latest live data file into blank environment
+  load(paste0("/echos/", i, "/glider_live.RData"),  gliders <- new.env())
   
   if (ahrCap$ahrCap > 0){
     msg <- envelope() %>%
-      emayili::render("/echos/batteryMarkdown.Rmd") %>%
+      emayili::render("/echos/batteryMarkdown.Rmd", .envir = gliders) %>%
       subject(paste0("Daily summary for ", as.character(i)))
     
     capture.output(print(msg, details = TRUE), file = paste0("/echos/", i, "/summary.html"))
   } else {
     msg <- envelope() %>%
-      emayili::render("/echos/batteryMarkdownNoAmp.Rmd") %>%
+      emayili::render("/echos/batteryMarkdownNoAmp.Rmd", .envir = gliders) %>%
       subject(paste0("Daily summary for ", as.character(i)))
     
     capture.output(print(msg, details = TRUE), file = paste0("/echos/", i, "/summary.html"))
