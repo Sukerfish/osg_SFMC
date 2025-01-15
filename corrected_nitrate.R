687310e67a7b693d42dfb919408add62d1fbc22e
Garrett Miller <47607889+oceanscienceguru@users.noreply.github.com>
2025-01-15 09:56:17 -0500

    added comments and clarified

--- corrected_nitrate.R
+++ corrected_nitrate.R
@@ -1,21 +1,21 @@
-#library(osgUtils)
-library(tidyverse)
-library(lubridate)
-#library(cmocean)
-#library(ggplot2)
-#library(patchwork)
+library(tidyverse) #formatting and syntax utilities
+library(lubridate) #date handling
 
+#load input glider dataset from Brewery
 load("./thebrewery/Data/M149_usf-jaialai.RData")
 
+#load MBARI NO3 data
 nitrate_corrected <- read.csv("M149_SUNA_MBARI_NO3.csv") %>%
-  mutate(m_present_time = as_datetime(DateTime))
+  mutate(m_present_time = as_datetime(DateTime)) #add column called m_present_time and format from DateTime
 
+#take Brewery data and calculate average water depth before merge
 output <- gliderdf %>%
   group_by(yo_id) %>%
   mutate(avg_water_depth = mean(m_water_depth, na.rm = TRUE)) %>%
-  ungroup() %>%
-  left_join(nitrate_corrected) %>%
-  select(m_present_time, Pres, Temp, Sal, NSBE, N, i_lat, i_lon, avg_water_depth) %>%
-  filter(!is.na(N))
+  ungroup() %>% 
+  left_join(nitrate_corrected, by = "m_present_time") %>% #join in MBARI data by m_present_time
+  select(m_present_time, Pres, Temp, Sal, NSBE, N, i_lat, i_lon, avg_water_depth) %>% #pick desired columns
+  filter(!is.na(N)) #drop all rows with NA values in column N
 
+#save the file as csv
 write.csv(output, "M149_SUNA_GPS_WDepths.csv", row.names = FALSE)
